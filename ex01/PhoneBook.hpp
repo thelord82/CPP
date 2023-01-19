750de076d6f163c2f641bc8e8109b7428f49e0fe
thelord82 <mandrelord@gmail.com>
2023-01-19 15:36:51 -0500

    ex01 in progress

---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 14:22:55 by malord            #+#    #+#             */
+/*   Updated: 2023/01/19 15:21:01 by malord           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_H
+# define PHONEBOOK_CLASS_H
+
+#include <iostream>
+#include <string>
+
+class PhoneBook {
+public:
+    Contact contacts[8];
+    PhoneBook(void);
+    ~PhoneBook(void);
+};
+
+class Contact {
+public:
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+    std::string phone_number;
+    std::string darkest_secret;
+    Contact(void);
+    ~Contact(void);
+};
+#endif
