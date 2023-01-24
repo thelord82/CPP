9a3d54e5baf8505fb4452fb1f1d7c503c4413f3a
thelord82 <mandrelord@gmail.com>
2023-01-24 15:58:36 -0500

    Added TODO

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,12 +6,13 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 10:54:41 by mal               #+#    #+#             */
-/*   Updated: 2023/01/24 15:56:21 by malord           ###   ########.fr       */
+/*   Updated: 2023/01/24 15:58:29 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
+//TODO Leak check
 int main(void)
 {
 	Contact instance;
