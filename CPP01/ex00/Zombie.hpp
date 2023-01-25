afea6961e3580c09379d623d5f250cc010d45f55
thelord82 <mandrelord@gmail.com>
2023-01-25 16:43:37 -0500

    Newfiles for C01

--- CPP01/ex00/Zombie.hpp
+++ CPP01/ex00/Zombie.hpp
@@ -6,12 +6,24 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 13:59:23 by malord            #+#    #+#             */
-/*   Updated: 2023/01/25 13:59:54 by malord           ###   ########.fr       */
+/*   Updated: 2023/01/25 14:06:52 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ZOMBIE_HPP_
 # define ZOMBIE_HPP_
 
+#include <iostream>
+#include <string>
+
+class Zombie
+{
+private:
+    std::string _name;/* data */
+public:
+    Zombie(/* args */);
+    ~Zombie();
+    void    announce(void);
+};
 
 #endif
--- Samples/main.cpp
+++ Samples/main.cpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 11:23:35 by malord            #+#    #+#             */
-/*   Updated: 2023/01/19 11:35:24 by malord           ###   ########.fr       */
+/*   Updated: 2023/01/25 14:37:29 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,27 @@
 
 int main()
 {
-    Sample instance;
+	Sample instance1(42);
+	Sample instance2(42);
 
-    std::cout << "foo = " << instance.foo << std::endl;
-    return (0);
+	if (&instance1 == &instance1)
+		std::cout << "instance 1 and instance 1 are physically equal" << std::endl;
+	else
+		std::cout << "instance 1 and instance 1 are not physically equal" << std::endl;
+	
+	if (&instance1 == &instance2)
+		std::cout << "instance 1 and instance 2 are physically equal" << std::endl;
+	else
+		std::cout << "instance 1 and instance 2 are not physically equal" << std::endl;
+	
+	if (instance1.compare(&instance1) == 0)
+		std::cout << "instance 1 and instance 1 are structurally equal" << std::endl;
+	else
+		std::cout << "instance 1 and instance 1 are not structurally equal" << std::endl;
+	
+	if (instance1.compare(&instance2) == 0)
+		std::cout << "instance 1 and instance 2 are structurally equal" << std::endl;
+	else
+		std::cout << "instance 1 and instance 2 are not structurally equal" << std::endl;
+	return (0);
 }
