398107b461a06468db0c591d2e259c95d81d4f8a
thelord82 <mandrelord@gmail.com>
2023-01-26 13:54:29 -0500

    Added numbers to zombies for identification

--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 13:09:21 by malord            #+#    #+#             */
-/*   Updated: 2023/01/26 13:41:14 by malord           ###   ########.fr       */
+/*   Updated: 2023/01/26 13:53:41 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ Zombie  *zombieHorde(int N, std::string name)
 {
     Zombie  *horde = new Zombie[N];
     for (int i = 0; i < N; i++)
-        horde[i].setZombieName(name);
+        horde[i].setZombieName(name + " " + std::to_string(i + 1));
     return (horde);
 }
