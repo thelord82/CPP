0c8d20306d12271aa7708fabd0318d0e8ffb74d7
thelord82 <mandrelord@gmail.com>
2023-02-09 16:20:12 -0500

    ex00 work in progress

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -6,15 +6,20 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 10:53:49 by malord            #+#    #+#             */
-/*   Updated: 2023/02/09 15:17:46 by malord           ###   ########.fr       */
+/*   Updated: 2023/02/09 16:17:03 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() : _hitPts(10), _energyPts(10), _attackDmg(0)
+ClapTrap::ClapTrap(void) : _hitPts(10), _energyPts(10), _attackDmg(0), _name("default")
 {
     std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name) : _name(name)
+{
+    std::cout << "Name constructor called" << std::endl;
 }
 
 ClapTrap::~ClapTrap()
@@ -22,3 +27,47 @@ ClapTrap::~ClapTrap()
     std::cout << "Default destructor called" << std::endl;
 }
 
+void ClapTrap::attack(const std::string &target)
+{
+    std::cout << "ClapTrap " << getName() << " attacks " << target << ", causing " << getAttackDmg()
+              << " points of damage!" << std::endl;
+    this->_energyPts--;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    this->_hitPts += amount;
+    this->_energyPts--;
+}
+
+std::string ClapTrap::getName(void) const
+{
+    return (this->_name);
+}
+
+int ClapTrap::getHitPts(void) const
+{
+    return (this->_hitPts);
+}
+
+int ClapTrap::getEnergyPts(void) const
+{
+    return (this->_energyPts);
+}
+
+int ClapTrap::getAttackDmg(void) const
+{
+    return (this->_attackDmg);
+}
+
+// void ClapTrap::setHitPts(ClapTrap player, unsigned int amount) {
+//     player._hitPts += amount;
+// }
+//
+// void ClapTrap::setEnergyPts(ClapTrap player, unsigned int amount) {
+//     player._energyPts += amount;
+// }
--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -1,16 +1,14 @@
-/* **************************************************************************
- */
+/* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      :::::::: */
-/*   ClapTrap.hpp                                       :+:      :+:    :+: */
-/*                                                    +:+ +:+         +:+ */
-/*   By: malord <malord@student.42.fr>              +#+  +:+       +#+ */
-/*                                                +#+#+#+#+#+   +#+ */
-/*   Created: 2023/02/09 10:53:46 by malord            #+#    #+# */
-/*   Updated: 2023/02/09 14:53:37 by malord           ###   ########.fr */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/09 16:08:36 by malord            #+#    #+#             */
+/*   Updated: 2023/02/09 16:12:57 by malord           ###   ########.fr       */
 /*                                                                            */
-/* **************************************************************************
- */
+/* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP_
 #define CLAPTRAP_HPP_
@@ -26,13 +24,22 @@ class ClapTrap
     int         _attackDmg;
 
   public:
-    ClapTrap();
+    ClapTrap(void);
+    ClapTrap(std::string name);
     ClapTrap(ClapTrap const &other);
     ClapTrap &operator=(ClapTrap const &rhs);
     ~ClapTrap();
     void attack(const std::string &target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
+    // Getters
+    std::string getName(void) const;
+    int         getHitPts(void) const;
+    int         getEnergyPts(void) const;
+    int         getAttackDmg(void) const;
+    // Setters #TODO Are those necessary?
+    // void setHitPts(ClapTrap player, unsigned int amount);
+    // void setEnergyPts(ClapTrap player, unsigned int amount);
 };
 
 #endif
