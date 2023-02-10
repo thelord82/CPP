668687db461a50d5ea6a8730ecf8c772228b3b99
thelord82 <mandrelord@gmail.com>
2023-02-10 12:58:54 -0500

    Added test

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 10:53:52 by malord            #+#    #+#             */
-/*   Updated: 2023/02/10 12:56:59 by malord           ###   ########.fr       */
+/*   Updated: 2023/02/10 12:58:02 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,4 +48,5 @@ int main()
 
     robot3.attack(robot2.getName());
     robot2.takeDamage(robot3.getAttackDmg());
+    robot2.guardGate();
 }
