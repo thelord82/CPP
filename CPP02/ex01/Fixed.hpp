b6b84e3bf8261b786ae03a5b950e4132561bb430
thelord82 <mandrelord@gmail.com>
2023-02-13 14:47:29 -0500

    Formatting

--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -6,43 +6,45 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 11:24:21 by malord            #+#    #+#             */
-/*   Updated: 2023/02/07 13:35:39 by malord           ###   ########.fr       */
+/*   Updated: 2023/02/13 14:04:02 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP_
 #define FIXED_HPP_
 
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
-class Fixed {
-    
-private:
-	int					_fixedNum;
-	static int const	_raw = 8;
-public:
-	//Constructors
-	Fixed(void);
-	Fixed(Fixed const & src);
-	Fixed(int const n);
-	Fixed(float const f);
-	
-	//Destructor
-	~Fixed(void);
-	
-	//Operator overload
-	Fixed & operator=(Fixed const & rhs);
+class Fixed
+{
 
-	//Getter function
-	int		getRawBits(void) const;
+  private:
+    int              _fixedNum;
+    static int const _raw = 8;
 
-	//Setters functions
-	void	setRawBits(int const raw);
-    float   toFloat(void) const;
-    int     toInt(void) const;
+  public:
+    // Constructors
+    Fixed(void);
+    Fixed(Fixed const &src);
+    Fixed(int const n);
+    Fixed(float const f);
+
+    // Destructor
+    ~Fixed(void);
+
+    // Operator overload
+    Fixed &operator=(Fixed const &rhs);
+
+    // Getter function
+    int getRawBits(void) const;
+
+    // Setters functions
+    void  setRawBits(int const raw);
+    float toFloat(void) const;
+    int   toInt(void) const;
 };
 
-std::ostream & operator<<(std::ostream & o, const Fixed & rhs);
+std::ostream &operator<<(std::ostream &o, const Fixed &rhs);
 
 #endif
