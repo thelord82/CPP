3155d12f1ef901031c8be63966af55451fdc8b29
thelord82 <mandrelord@gmail.com>
2023-02-14 11:04:40 -0500

    Implemented classes

--- CPP04/ex01/Brain.cpp
+++ CPP04/ex01/Brain.cpp
@@ -6,7 +6,29 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 09:54:35 by malord            #+#    #+#             */
-/*   Updated: 2023/02/14 09:54:36 by malord           ###   ########.fr       */
+/*   Updated: 2023/02/14 10:19:46 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Brain.hpp"
+
+Brain::Brain()
+{
+    std::cout << "Brain constructor called" << std::endl;
+}
+
+Brain::Brain(Brain const &copy)
+{
+    *this = copy;
+}
+
+Brain &Brain::operator=(Brain const &rhs)
+{
+    //TODO fill this
+    return (*this);
+}
+
+Brain::~Brain()
+{
+    std::cout << "Brain destructor called" << std::endl;
+}
--- CPP04/ex01/Brain.hpp
+++ CPP04/ex01/Brain.hpp
@@ -6,7 +6,26 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 09:54:33 by malord            #+#    #+#             */
-/*   Updated: 2023/02/14 09:54:34 by malord           ###   ########.fr       */
+/*   Updated: 2023/02/14 10:20:16 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BRAIN_HPP_
+#define BRAIN_HPP_
+#include <iostream>
+#include <string>
+
+class Brain
+{
+  private:
+    /* data */
+  public:
+    Brain();
+    Brain(Brain const &copy);
+    Brain &operator=(Brain const &rhs);
+    ~Brain();
+
+    std::string ideas[100];
+};
+
+#endif
