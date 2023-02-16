299723bdaf50c3c285b8ea01deebc015fd9ca0df
thelord82 <mandrelord@gmail.com>
2023-02-16 14:53:07 -0500

    ex00 done

--- CPP05/ex00/Exception.cpp
+++ CPP05/ex00/Exception.cpp
@@ -6,7 +6,18 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 13:32:59 by malord            #+#    #+#             */
-/*   Updated: 2023/02/16 13:46:38 by malord           ###   ########.fr       */
+/*   Updated: 2023/02/16 14:52:50 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Exception.hpp"
+
+const char *MyException::GradeTooLowException::what() const throw()
+{
+    return ("Grade can't be lower than 150.");
+}
+
+const char *MyException::GradeTooHighException::what() const throw()
+{
+    return ("Grade can't be higher than 1.");
+}
