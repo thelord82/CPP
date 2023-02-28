0a90981e284f8cdadea8e9d023f298edec792772
thelord82 <mandrelord@gmail.com>
2023-02-28 13:51:38 -0500

    ex01 done

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 10:59:55 by malord            #+#    #+#             */
-/*   Updated: 2023/02/28 11:51:46 by malord           ###   ########.fr       */
+/*   Updated: 2023/02/28 13:51:16 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,22 @@
 
 int main(void)
 {
-    Data instanceTest;
+    Data *instanceTest = NULL;
     uintptr_t test;
     Data *instanceDeserialized = NULL;
 
-    test = instanceDeserialized->serialize(&instanceTest);
+    test = instanceDeserialized->serialize(instanceTest);
     instanceDeserialized = instanceDeserialized->deserialize(test);
     std::cout << test << " vs " << instanceDeserialized << std::endl;
-    if (instanceDeserialized == &instanceTest)
+    if (instanceDeserialized == instanceTest)
         std::cout << "WORKS BITCH!" << std::endl;
     else
         std::cout << "THIS NOT IT DUMBFUCK" << std::endl;
-    ++instanceDeserialized;
-    std::cout << test << " vs " << instanceDeserialized << std::endl;
-    if (instanceDeserialized == &instanceTest)
+    if (++instanceDeserialized == instanceTest)
         std::cout << "OF COURSE NOT YOU DUMBFUCK!" << std::endl;
     else
+    {
+        std::cout << test << " vs " << instanceDeserialized << std::endl;
         std::cout << "THERE YOU GO!" << std::endl;
+    }
 }
