6ce11ef545038da8b87176ab8bac6ef1403a1361
thelord82 <mandrelord@gmail.com>
2023-03-02 09:29:33 -0500

    ex00 finished and polished

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 14:15:58 by malord            #+#    #+#             */
-/*   Updated: 2023/03/02 09:24:53 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/02 09:29:09 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ int main(void)
 
     std::cout << std::endl << "TEST WITH INTEGERS" << std::endl;
     std::cout << "Avant swap : " << a << ", " << b << std::endl;
-    swap<int>(a, b);
+    ::swap<int>(a, b);
     std::cout << "Apres swap : " << a << ", " << b << std::endl;
 
-    std::cout << "Le min entre " << a << " et " << b << " est " << min<int>(a, b) << std::endl;
-    std::cout << "Le max entre " << a << " et " << b << " est " << max<int>(a, b) << std::endl;
+    std::cout << "Le min entre " << a << " et " << b << " est " << ::min<int>(a, b) << std::endl;
+    std::cout << "Le max entre " << a << " et " << b << " est " << ::max<int>(a, b) << std::endl;
     std::cout << std::endl << "--------------------" << std::endl << std::endl;
 
     float c = 42.0f;
@@ -32,22 +32,22 @@ int main(void)
     std::cout << "TEST WITH FLOAT" << std::endl;
     std::cout << std::setprecision(1) << std::fixed;
     std::cout << "Avant swap : " << c << ", " << d << std::endl;
-    swap<float>(c, d);
+    ::swap<float>(c, d);
     std::cout << "Apres swap : " << c << ", " << d << std::endl;
 
-    std::cout << "Le min entre " << c << " et " << d << " est " << min<float>(c, d) << std::endl;
-    std::cout << "Le max entre " << c << " et " << d << " est " << max<float>(c, d) << std::endl;
+    std::cout << "Le min entre " << c << " et " << d << " est " << ::min<float>(c, d) << std::endl;
+    std::cout << "Le max entre " << c << " et " << d << " est " << ::max<float>(c, d) << std::endl;
     std::cout << std::endl << "--------------------" << std::endl << std::endl;
 
     char e = 'a';
     char f = 'z';
     std::cout << "TEST WITH CHAR" << std::endl;
     std::cout << "Avant swap : " << e << ", " << f << std::endl;
-    swap<char>(e, f);
+    ::swap<char>(e, f);
     std::cout << "Apres swap : " << e << ", " << f << std::endl;
 
-    std::cout << "Le min entre " << e << " et " << f << " est " << min<char>(e, f) << std::endl;
-    std::cout << "Le max entre " << e << " et " << f << " est " << max<char>(e, f) << std::endl;
+    std::cout << "Le min entre " << e << " et " << f << " est " << ::min<char>(e, f) << std::endl;
+    std::cout << "Le max entre " << e << " et " << f << " est " << ::max<char>(e, f) << std::endl;
     std::cout << std::endl << "--------------------" << std::endl << std::endl;
 
     double g = 21.0;
@@ -56,11 +56,11 @@ int main(void)
     std::cout << "TEST WITH DOUBLES" << std::endl;
     std::cout << std::setprecision(1) << std::fixed;
     std::cout << "Avant swap : " << g << ", " << h << std::endl;
-    swap<double>(g, h);
+    ::swap<double>(g, h);
     std::cout << "Apres swap : " << g << ", " << h << std::endl;
 
-    std::cout << "Le min entre " << g << " et " << h << " est " << min<double>(g, h) << std::endl;
-    std::cout << "Le max entre " << g << " et " << h << " est " << max<double>(g, h) << std::endl;
+    std::cout << "Le min entre " << g << " et " << h << " est " << ::min<double>(g, h) << std::endl;
+    std::cout << "Le max entre " << g << " et " << h << " est " << ::max<double>(g, h) << std::endl;
     std::cout << std::endl << "--------------------" << std::endl << std::endl;
 
     std::string str1 = "drink";
@@ -79,8 +79,8 @@ int main(void)
     int j = 69;
 
     std::cout << "TEST WITH EQUAL VALUES" << std::endl;
-    std::cout << "Le min entre " << i << " et " << j << " est " << min<int>(i, j) << std::endl;
-    std::cout << "Le max entre " << i << " et " << j << " est " << max<int>(i, j) << std::endl << std::endl;
+    std::cout << "Le min entre " << i << " et " << j << " est " << ::min<int>(i, j) << std::endl;
+    std::cout << "Le max entre " << i << " et " << j << " est " << ::max<int>(i, j) << std::endl << std::endl;
 }
 
 // SUBJECT'S MAIN TEST
