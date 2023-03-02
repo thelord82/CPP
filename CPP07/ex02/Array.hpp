f753f97dab942e3e258bac1d68aa0ee18c30cc5c
thelord82 <mandrelord@gmail.com>
2023-03-02 13:47:04 -0500

    ex02 work in progress

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 13:00:29 by malord            #+#    #+#             */
-/*   Updated: 2023/03/02 13:31:38 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/02 13:46:39 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,48 @@
 template <typename T> class Array
 {
   private:
-    T *_elements;
+    T           *_elements;
+    unsigned int _size;
 
   public:
     Array(void);
     Array(unsigned int n);
-    Array(Array const &rhs copy);
-    Array &operator=(Array const &rhs);
+    Array(const Array &copy);
+    Array &operator=(const Array &rhs);
     ~Array(void);
 };
 
-template<typename T>
-Array<T>::Array(void)
+template <typename T> Array<T>::Array(void)
 {
     std::cout << "Default constructor called" << std::endl;
     _elements = NULL;
 }
 
-template<typename T>
-Array<T>::~Array(void)
+template <typename T>
+Array<T>::Array(unsigned int n) : new T[n], _size(n)
+{
+    std::cout << "Parameter constructor called" << std::endl;
+}
+
+template <typename T>
+Array<T>::Array(const Array &copy)
+{
+    this->_elements = copy._elements;
+    this->_size = copy._size;
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array &rhs)
+{
+    if(this != &rhs)
+    {
+        this->_size = rhs._size;
+        this->_elements = rhs._elements;   
+    }
+    return (*this);
+}
+
+template <typename T> Array<T>::~Array(void)
 {
     delete _elements[];
 }
