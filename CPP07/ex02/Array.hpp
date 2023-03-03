4abe5a8925d01e56385e7cb57f08036a89ca5ce4
thelord82 <mandrelord@gmail.com>
2023-03-03 10:27:41 -0500

    ex02 work in progresszx

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 13:00:29 by malord            #+#    #+#             */
-/*   Updated: 2023/03/02 13:46:39 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/03 10:27:15 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,42 +27,56 @@ template <typename T> class Array
     Array(unsigned int n);
     Array(const Array &copy);
     Array &operator=(const Array &rhs);
+    T     &operator[](unsigned int index);
     ~Array(void);
 };
 
-template <typename T> Array<T>::Array(void)
+template <typename T> Array<T>::Array(void) : _elements(NULL), _size(0)
 {
     std::cout << "Default constructor called" << std::endl;
-    _elements = NULL;
 }
 
-template <typename T>
-Array<T>::Array(unsigned int n) : new T[n], _size(n)
+template <typename T> Array<T>::Array(unsigned int n) : _elements(new T[n]()), _size(n)
 {
     std::cout << "Parameter constructor called" << std::endl;
 }
 
-template <typename T>
-Array<T>::Array(const Array &copy)
+template <typename T> Array<T>::Array(const Array<T> &copy) : _elements(new T[copy._size]()), _size(copy._size)
 {
-    this->_elements = copy._elements;
-    this->_size = copy._size;
+    for (int i = 0; i < _size; ++i)
+        _elements[i] = copy._elements[i];
 }
 
-template <typename T>
-Array<T> &Array<T>::operator=(const Array &rhs)
+template <typename T> Array<T> &Array<T>::operator=(const Array<T> &rhs)
 {
-    if(this != &rhs)
+    if (this != &rhs)
     {
-        this->_size = rhs._size;
-        this->_elements = rhs._elements;   
+        delete[] _elements;
+        _elements = new T[rhs._size];
+        _size     = rhs._size;
+        for (int i = 0; i < rhs._size; ++i)
+            _elements[i] = rhs._elements[i];
     }
     return (*this);
 }
 
+template <typename T> T &Array<T>::operator[](unsigned int index)
+{
+    try
+    {
+        if (index >= _size)
+            throw std::out_of_range("Index of the array is out of range (bigger than size)");
+    }
+    catch (std::exception &oor)
+    {
+        std::cerr << oor.what() << std::endl;
+    }
+    return (_elements[index]);
+}
+
 template <typename T> Array<T>::~Array(void)
 {
-    delete _elements[];
+    delete[] _elements;
 }
 
 #endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,13 +6,28 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 13:00:33 by malord            #+#    #+#             */
-/*   Updated: 2023/03/02 13:00:54 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/03 10:20:33 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
+//int main(void)
+//{
+//    Array<int> a(3);
+//    for (int i = 0; i < 3; ++i)
+//        a[i] = 3;
+//    for (int i = 0; i < 3; ++i)
+//        std::cout << "a[" << i << "] = " << a[i] << std::endl;
+//    return 0;
+//}
+
 int main(void)
 {
+    Array<int> a(3);
+    for (int i = 0; i < 5; ++i)
+        a[i] = 3;
+    for (int i = 0; i < 5; ++i)
+        std::cout << "a[" << i << "] = " << a[i] << std::endl;
     return 0;
 }
