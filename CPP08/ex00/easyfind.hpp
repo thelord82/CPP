e8f6ef4848f254abe3cc2efff6b86b7ca16550e2
thelord82 <mandrelord@gmail.com>
2023-03-08 10:12:58 -0500

    ex00 done

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 11:04:47 by malord            #+#    #+#             */
-/*   Updated: 2023/03/06 15:24:12 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/08 10:12:45 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <deque>
+#include <stack>
 
 template <typename T> typename T::iterator easyfind(T first, int second)
 {
@@ -31,52 +33,3 @@ template <typename T> typename T::iterator easyfind(T first, int second)
 }
 
 #endif
-
-// Implementation d'iterateurs generique
-
-/*
-#include <iostream>
-#include <list>
-#include <vector>
-
-template<typename Container>
-void print_container(const Container& container)
-{
-    // Define a generic iterator that works for both std::vector and std::list
-    typedef typename Container::value_type value_type;
-    typedef typename Container::iterator iterator_type;
-    typedef typename Container::const_iterator const_iterator_type;
-
-    typedef typename std::iterator_traits<iterator_type>::iterator_category iterator_category;
-    typedef typename std::iterator_traits<const_iterator_type>::iterator_category const_iterator_category;
-
-    typedef typename std::conditional<
-        std::is_same<iterator_category, std::random_access_iterator_tag>::value
-        && std::is_same<const_iterator_category, std::random_access_iterator_tag>::value,
-        iterator_type, const_iterator_type>::type generic_iterator;
-
-    // Print the elements of the container using the iterator
-    for (generic_iterator it = container.begin(); it != container.end(); ++it) {
-        std::cout << *it << " ";
-    }
-
-    std::cout << std::endl;
-}
-
-int main()
-{
-    std::vector<int> v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    print_container(v);
-
-    std::list<double> l;
-    l.push_back(4.0);
-    l.push_back(5.0);
-    l.push_back(6.0);
-    print_container(l);
-
-    return 0;
-}
-*/
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,35 +6,76 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 10:35:51 by malord            #+#    #+#             */
-/*   Updated: 2023/03/06 15:19:36 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/08 10:08:44 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
-
 int main(void)
 {
-    //TODO Bonifier le main pour que ca ait de l'allure
-    
-    //std::vector<int> v1;
-    //v1.push_back(120);
-    //v1.push_back(18);
-    //v1.push_back(22);
-    std::list<int> lst1;
+
+    // Test with a list container
+    std::list<int>                   lst1;
     lst1.push_back(10);
     lst1.push_back(12);
     lst1.push_back(14);
-    std::list<int>::iterator result;
-
-    try 
+    std::list<int>::const_iterator resultL;
+    std::cout << "------------------" << std::endl;
+    std::cout << "LIST CONTAINER" << std::endl;
+    std::cout << "------------------" << std::endl;
+    try
     {
-        result = easyfind(lst1, 12);
+        resultL = easyfind(lst1, 12);
     }
     catch (std::exception &e)
     {
         std::cerr << "Error: value wasn't found." << std::endl;
         return (0);
     }
-    std::cout << "result = " << *result << std::endl;
+    std::cout << "result = " << *resultL << std::endl;
+
+    // Test with a vector container
+    std::vector<int> v1;
+    v1.push_back(120);
+    v1.push_back(18);
+    v1.push_back(22);
+    std::vector<int>::const_iterator resultV;
+    std::cout << std::endl;
+    std::cout << "------------------" << std::endl;
+    std::cout << "VECTOR CONTAINER" << std::endl;
+    std::cout << "------------------" << std::endl;
+    try
+    {
+        resultV = easyfind(v1, 120);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Error: value wasn't found." << std::endl;
+        return (0);
+    }
+    std::cout << "result = " << *resultV << std::endl;
+    
+    // Test with a deque container
+    std::deque<int> d1;
+    d1.push_back(120);
+    d1.push_back(18);
+    d1.push_back(22);
+    std::deque<int>::const_iterator resultD;
+    std::cout << std::endl;
+    std::cout << "------------------" << std::endl;
+    std::cout << "DEQUE CONTAINER" << std::endl;
+    std::cout << "------------------" << std::endl;
+    try
+    {
+        resultD = easyfind(d1, 120);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Error: value wasn't found." << std::endl;
+        return (0);
+    }
+    std::cout << "result = " << *resultD << std::endl;
+    
+    std::cout << std::endl;
     return (0);
 }
