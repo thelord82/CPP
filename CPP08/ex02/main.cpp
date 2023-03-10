c7421d67eaabcc9ac97f4bbf6e6d389790b8cba9
thelord82 <mandrelord@gmail.com>
2023-03-10 15:11:32 -0500

    ex02 work in progress, iterators not done

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:24:20 by malord            #+#    #+#             */
-/*   Updated: 2023/03/10 15:09:12 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/10 15:11:17 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,38 +19,41 @@ int main()
     mstack.push(5);
     mstack.push(17);
     std::cout << mstack.top() << std::endl;
-    //mstack.pop();
+    // mstack.pop();
     std::cout << mstack.size() << std::endl;
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
     //[...]
     mstack.push(0);
-    std::vector<int> tempVec;
-    while (!mstack.empty())
-    {
-        tempVec.push_back(mstack.top());
-        mstack.pop();
-    }
-    
-    for (std::vector<int>::const_iterator it = tempVec.begin(); it != tempVec.end(); ++it)
-        std::cout << "Value = " << *it << std::endl;
-
-    //Displays and pop the contents of the stack
-    //while (!mstack.empty())
+    // Displays and pop the contents of the stack
+    // while (!mstack.empty())
     //{
-    //    std::cout << "value = " << mstack.top() << std::endl;
-    //    mstack.pop();
-    //}
-    //MutantStack<int>::iterator it  = mstack.begin();
-    //MutantStack<int>::iterator ite = mstack.end();
+    //     std::cout << "value = " << mstack.top() << std::endl;
+    //     mstack.pop();
+    // }
+    
+    // Another way to print the content of the stack
+    // std::vector<int> tempVec;
+    // while (!mstack.empty())
+    //{
+    //     tempVec.push_back(mstack.top());
+    //     mstack.pop();
+    // }
+    //
+    // for (std::vector<int>::const_iterator it = tempVec.begin(); it != tempVec.end(); ++it)
+    //     std::cout << "Value = " << *it << std::endl;
+
+    
+    // MutantStack<int>::iterator it  = mstack.begin();
+    // MutantStack<int>::iterator ite = mstack.end();
     //++it;
     //--it;
-    //while (it != ite)
+    // while (it != ite)
     //{
-    //    std::cout << *it << std::endl;
-    //    ++it;
-    //}
-    //std::stack<int> s(mstack);
+    //     std::cout << *it << std::endl;
+    //     ++it;
+    // }
+    // std::stack<int> s(mstack);
     return 0;
 }
