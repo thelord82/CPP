29ce6e04c9eb229c8f919ea70530a08ab7d9bf36
thelord82 <mandrelord@gmail.com>
2023-03-13 11:49:18 -0400

    begin and end iterators full implemented

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:24:18 by malord            #+#    #+#             */
-/*   Updated: 2023/03/13 11:44:10 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/13 11:48:07 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,12 @@ class MutantStack : public std::stack<T, std::deque<T> >
 {
     public:
         typedef typename std::deque<T>::iterator iterator;
-        //typedef typename std::deque<const T>::const_iterator const_iterator;
+        typedef typename std::deque<T>::const_iterator const_iterator;
 
         iterator begin(void);
         iterator end(void);
-        //const_iterator begin(void);
-        //const_iterator end(void);
+        const_iterator begin(void) const;
+        const_iterator end(void) const;
 };
 
 #include "MutantStack.tpp"
--- CPP08/ex02/MutantStack.tpp
+++ CPP08/ex02/MutantStack.tpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:24:15 by malord            #+#    #+#             */
-/*   Updated: 2023/03/13 11:44:02 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/13 11:48:44 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,18 @@ typename std::deque<T>::iterator MutantStack<T>::begin(void)
 
 template <typename T>
 typename std::deque<T>::iterator MutantStack<T>::end(void)
+{
+    return (this->c.end());
+}
+
+template <typename T>
+typename std::deque<T>::const_iterator MutantStack<T>::begin(void) const
+{
+    return (this->c.begin());
+}
+
+template <typename T>
+typename std::deque<T>::const_iterator MutantStack<T>::end(void) const
 {
     return (this->c.end());
 }
