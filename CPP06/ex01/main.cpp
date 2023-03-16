b9904101be7ee835aef33080c803c8db239842f9
thelord82 <mandrelord@gmail.com>
2023-03-16 10:39:34 -0400

    Added test for polishing

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 10:59:55 by malord            #+#    #+#             */
-/*   Updated: 2023/02/28 13:51:16 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/16 10:39:19 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,22 @@ int main(void)
 
     test = instanceDeserialized->serialize(instanceTest);
     instanceDeserialized = instanceDeserialized->deserialize(test);
-    std::cout << test << " vs " << instanceDeserialized << std::endl;
+    std::cout << "test = " << test << " vs " << "deserialized = " << instanceDeserialized << std::endl;
     if (instanceDeserialized == instanceTest)
         std::cout << "WORKS BITCH!" << std::endl;
     else
         std::cout << "THIS NOT IT DUMBFUCK" << std::endl;
+    Data *copyInstance(instanceTest);
+    if (copyInstance == instanceDeserialized)
+        std::cout << "COPY WORKS" << std::endl;
+    Data *equalInstance = instanceTest;
+    if (equalInstance == instanceTest)
+        std::cout << "EQUAL WORKS" << std::endl;
     if (++instanceDeserialized == instanceTest)
         std::cout << "OF COURSE NOT YOU DUMBFUCK!" << std::endl;
     else
     {
-        std::cout << test << " vs " << instanceDeserialized << std::endl;
+        std::cout << "test = " << test << " vs " << "deserialized = " << instanceDeserialized << std::endl;
         std::cout << "THERE YOU GO!" << std::endl;
     }
 }
