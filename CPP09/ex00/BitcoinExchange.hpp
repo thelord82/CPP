203578a10d37b1ab66238d5731b6a85869ef9b9b
thelord82 <mandrelord@gmail.com>
2023-03-17 11:23:41 -0400

    ex00 start of header file

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,30 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 14:17:19 by malord            #+#    #+#             */
-/*   Updated: 2023/03/14 14:17:20 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/17 11:23:14 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BITCOINEXCHANGE_HPP_
+#define BITCOINEXCHANGE_HPP_
+
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+class Data
+{
+    private:
+        std::vector<std::string> _dbDates; // stores dates from DB
+        std::vector<float> _exchangeValue; // stores value from DB
+    public:
+        Data(void);
+        Data(const Data &copy);
+        Data &operator=(const Data &rhs);
+        virtual ~Data(void);
+        bool dateChecker(std::string date); // checks valid date from input file
+};
+#endif
