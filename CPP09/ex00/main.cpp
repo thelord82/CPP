b0f1039b2adba9fdb58270eb9fb9e125f226faec
thelord82 <mandrelord@gmail.com>
2023-03-17 08:07:59 -0400

    Added code for getting the correct date format for parsing

--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -6,7 +6,32 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 14:17:17 by malord            #+#    #+#             */
-/*   Updated: 2023/03/14 14:17:18 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/17 08:01:35 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+int main()
+{
+    
+    // THIS is the date handling function
+    //std::string date_str = "2022-12-33";
+    //std::tm     date     = {};
+//
+    //std::istringstream ss(date_str);
+    //ss >> std::get_time(&date, "%Y-%m-%d");
+//
+    //if (ss.fail())
+    //{
+    //    std::cout << "Error: Invalid date string\n";
+    //    return (1);
+    //}
+//
+    //std::cout << "Date: " << std::put_time(&date, "%Y-%m-%d") << "\n";
+
+    return 0;
+}
