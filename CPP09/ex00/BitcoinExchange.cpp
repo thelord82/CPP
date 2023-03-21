8b0dc1c2887172b391c9c6e37988df1ebb06635b
thelord82 <mandrelord@gmail.com>
2023-03-21 10:51:20 -0400

    Added value checking and TODO

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 14:17:21 by malord            #+#    #+#             */
-/*   Updated: 2023/03/21 10:44:14 by malord           ###   ########.fr       */
+/*   Updated: 2023/03/21 10:51:12 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,8 +62,8 @@ void Data::fillDatabase(void)
         }
     }
     // THIS prints the content of list coming from the csv data base file
-    //int i = 1;
-    //for (std::list<std::pair<std::string, float>>::iterator it = this->dataBase.begin(); it != this->dataBase.end();
+    // int i = 1;
+    // for (std::list<std::pair<std::string, float>>::iterator it = this->dataBase.begin(); it != this->dataBase.end();
     //     ++it)
     //{
     //    std::cout << i << " LIST = " << it->first << " et " << it->second << std::endl;
@@ -90,25 +90,32 @@ void Data::fillInput(std::string inputFile)
             position = line.find(toSplitOn);
             value    = std::stof(line.substr(position + 1, line.length()));
         }
-        this->inputFile.push_back(std::make_pair<std::string, float>(static_cast<std::string>(date), static_cast<float>(value)));
+        this->inputFile.push_back(
+            std::make_pair<std::string, float>(static_cast<std::string>(date), static_cast<float>(value)));
     }
     // THIS prints the content of the list coming from the input file
-     for (std::list<std::pair<std::string, float> >::iterator it = this->inputFile.begin(); it !=
-     this->inputFile.end(); ++it)
-     {
+    for (std::list<std::pair<std::string, float> >::iterator it = this->inputFile.begin(); it != this->inputFile.end();
+         ++it)
+    {
         if (!validateDate(it->first))
             std::cout << "Error: bad input => " << it->first << std::endl;
+        else if (it->second < 0)
+            std::cout << "Error: Not a positive number" << std::endl;
+        else if (it->second > 1000)
+            std::cout << "Error: too large a number";
         else
             std::cout << it->first << " => " << it->second << std::endl;
-     }
+    }
 }
 
 bool Data::validateDate(std::string strDate)
 {
-        std::tm date = {};
-        std::istringstream ss(strDate);
-        ss >> std::get_time(&date, "%Y-%m-%d");
-        if (ss.fail())
-            return (false);
-        return (true);
+    std::tm            date = {};
+    std::istringstream ss(strDate);
+    ss >> std::get_time(&date, "%Y-%m-%d");
+    if (ss.fail())
+        return (false);
+    return (true);
 }
+
+//TODO Prendre les 2 valeurs de inputFile et dataBase et les multiplier ensemble si pas d'erreurs.
