412ede2a553342e53b405f49319b029a1d536dad
thelord82 <mandrelord@gmail.com>
2023-04-04 15:10:27 -0400

    Templating in progress + refactoring

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 14:20:17 by malord            #+#    #+#             */
-/*   Updated: 2023/04/04 12:11:46 by malord           ###   ########.fr       */
+/*   Updated: 2023/04/04 15:09:47 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ PmergeMe::PmergeMe(void)
     // std::cout << "Default constructor called" << std::endl;
 }
 
-PmergeMe::PmergeMe(const PmergeMe &copy) : _rawData(copy._rawData), _pairDeq(copy._pairDeq)
+PmergeMe::PmergeMe(const PmergeMe &copy) : _rawData(copy._rawData), _deq(copy._deq)
 {
     *this = copy;
 }
@@ -27,7 +27,7 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &rhs)
     if (this != &rhs)
     {
         this->_rawData = rhs._rawData;
-        this->_pairDeq  = rhs._pairDeq;
+        this->_deq  = rhs._deq;
     }
     return (*this);
 }
@@ -37,6 +37,7 @@ PmergeMe::~PmergeMe(void)
     // std::cout << "Default destructor called" << std::endl;
 }
 
+// THIS may need a refactor to be less clunky
 bool PmergeMe::checkNumbers(int argc, char **argv)
 {
     if (argc <= 1)
@@ -83,65 +84,35 @@ bool PmergeMe::checkNumbers(int argc, char **argv)
 // THIS pairs the data and controls if the number of elements is odd
 void PmergeMe::pairData(void)
 {
-    std::unordered_multimap<int, int> tmpMap;
     for (std::vector<int>::iterator it = _rawData.begin(); it != _rawData.end(); ++it)
     {
-        if (std::next(it) != _rawData.end())
+            _vec.push_back(*it);
+            _deq.push_back(*it);
+    }
+    swapData(_vec.begin(), _vec.end());
+    swapData(_deq.begin(), _deq.end());
+    printContainer(_vec.begin(), _vec.end()); // Will be removed
+    std::cout << "------------" << std::endl; // Will be removed
+    printContainer(_deq.begin(), _deq.end()); // Will be removed
+}
+
+template <typename Iterator>void printContainer(Iterator begin, Iterator end)
+{
+    for (Iterator it = begin; it != end; ++it)
+        std::cout << "content = " << *it << std::endl;
+}
+
+template <typename Iterator>void swapData(Iterator begin, Iterator end)
+{
+    for (Iterator it = begin; it != end; ++it)
+    {
+        if (*it > *(it + 1) && (it + 1) != end)
         {
-            _pairVec.push_back(std::make_pair(*it, *(it + 1)));
-            _pairDeq.push_back(std::make_pair(*it, *(it + 1)));
-            //_pairMM.insert(std::make_pair(*it, *(it + 1)));
+            std::swap(*it, *(it + 1));
             ++it;
         }
-        else
-        {
-            _pairVec.push_back(std::make_pair(*it, -1));
-            _pairDeq.push_back(std::make_pair(*it, -1));
-            //_pairMM.insert(std::make_pair(*it, -1));
-        }
     }
-    for (std::vector<std::pair<int, int> >::iterator it = _pairVec.begin(); it != _pairVec.end(); ++it)
-    {
-        if (it->first > it->second && it->second >= 0)
-            std::swap(it->first, it->second);
-    }
-
-    for (std::deque<std::pair<int, int> >::iterator it = _pairDeq.begin(); it != _pairDeq.end(); ++it)
-    {
-        if (it->first > it->second && it->second >= 0)
-            std::swap(it->first, it->second);
-    }
-
-    // THIS is a print test
-    for (std::vector<std::pair<int, int> >::iterator it = _pairVec.begin(); it != _pairVec.end(); ++it)
-        std::cout << "VEC BEFORE SWAP = " << it->first << " et " << it->second << std::endl;
-
-    for (std::deque<std::pair<int, int> >::iterator it = _pairDeq.begin(); it != _pairDeq.end(); ++it)
-        std::cout << "DEQ BEFORE SWAP = " << it->first << " et " << it->second << std::endl;
-
-    // THIS sorts the vector according to the second value
-    std::sort(_pairVec.begin(), _pairVec.end(), compSecond);
-    std::sort(_pairDeq.begin(), _pairDeq.end(), compSecond);
-    
-    // THIS prints the result after the swap
-    for (std::vector<std::pair<int, int> >::iterator it = _pairVec.begin(); it != _pairVec.end(); ++it)
-        std::cout << "VEC AFTER SWAP = " << it->first << " et " << it->second << std::endl;
-
-    for (std::deque<std::pair<int, int> >::iterator it = _pairDeq.begin(); it != _pairDeq.end(); ++it)
-        std::cout << "DEQ AFTER SWAP = " << it->first << " et " << it->second << std::endl;
 }
 
-bool compSecond(std::pair<int, int> &p1, std::pair<int, int> &p2)
-{
-    if (p1.second == -1)
-        return (false);
-    return p1.second < p2.second;
-}
-
-//TODO To template or not to template, that is the question.
-//REFACTOR functions
-
-//TODO use std::transform to separate the pairs of _pairVec vector. 
-//TODO insert the first values at the right place in the second vector, respecting the algorithm
-//TODO check if I don't do too much manipulations for nothing
-//!GETTING THERE BUD!
+//TODO Templating in progress...
+ 
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 14:20:19 by malord            #+#    #+#             */
-/*   Updated: 2023/04/04 12:03:50 by malord           ###   ########.fr       */
+/*   Updated: 2023/04/04 14:22:49 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@
 #include <string>
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 #include <deque>
 class PmergeMe
 {
 private:
     std::vector<int> _rawData;
-    std::vector<std::pair<int, int> > _pairVec;   
-    std::deque<std::pair<int, int> > _pairDeq;
+    std::vector<int> _vec;
+    std::deque<int> _deq;
+    //std::vector<std::pair<int, int> > _pairVec;   
+    //std::deque<std::pair<int, int> > _pairDeq;
 public:
     PmergeMe(void);
     PmergeMe(const PmergeMe &copy);
@@ -33,6 +34,28 @@ public:
     bool checkNumbers(int argc, char **argv);
     void pairData(void);
 };
-    bool compSecond(std::pair<int, int>& p1, std::pair<int, int>& p2);
+
+template <typename Iterator>
+void printContainer(Iterator begin, Iterator end);
+
+template <typename Iterator>
+void swapData(Iterator begin, Iterator end);
+
+//template <typename Container>
+//class PmergeMe
+//{
+//    private:
+//        std::vector<int> _rawData;
+//        Container _pairs;
+//    public:
+//        PmergeMe(void);
+//        PmergeMe(const PmergeMe &copy);
+//        PmergeMe &operator=(const PmergeMe &rhs);
+//        virtual ~PmergeMe(void);
+//
+//        bool checkNumbers(int argc, char **argv);
+//        void pairData(void);
+//};
+//bool compSecond(std::pair<int, int>& p1, std::pair<int, int> &p2);
 
 #endif
