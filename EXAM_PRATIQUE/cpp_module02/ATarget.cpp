97e9e2127b1f32e1a74a4d8ff5a22b8c28b114aa
Marc-Andre <mandrelord@gmail.com>
2023-04-17 23:25:16 -0400

    Still unknown segfault

--- EXAM_PRATIQUE/cpp_module02/ATarget.cpp
+++ EXAM_PRATIQUE/cpp_module02/ATarget.cpp
@@ -6,13 +6,13 @@
 /*   By: mal <mal@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 11:16:10 by malord            #+#    #+#             */
-/*   Updated: 2023/04/17 19:49:59 by mal              ###   ########.fr       */
+/*   Updated: 2023/04/17 23:02:16 by mal              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ATarget.hpp"
 
-ATarget::ATarget(void){}
+ATarget::ATarget(void) : _type(){}
 
 ATarget::ATarget(const ATarget &copy) : _type(copy._type)
 {
--- EXAM_PRATIQUE/cpp_module02/TargetGenerator.cpp
+++ EXAM_PRATIQUE/cpp_module02/TargetGenerator.cpp
@@ -6,7 +6,7 @@
 /*   By: mal <mal@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 21:26:53 by mal               #+#    #+#             */
-/*   Updated: 2023/04/17 21:38:37 by mal              ###   ########.fr       */
+/*   Updated: 2023/04/17 23:17:27 by mal              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,22 @@
 
 TargetGenerator::TargetGenerator(void){}
 
-TargetGenerator::~TargetGenerator(void){}
+TargetGenerator::~TargetGenerator(void){
+	std::vector<ATarget*>::iterator ite = this->_types.end();
+	for (std::vector<ATarget*>::iterator it = this->_types.begin(); it != ite; ++it)
+		delete *it;
+	this->_types.clear();
+}
 
 void TargetGenerator::learnTargetType(ATarget *type)
 {
     if (type) {
 		std::vector<ATarget*>::iterator ite = _types.end();
 		for (std::vector<ATarget*>::iterator it = _types.begin(); it != ite; ++it)
+		{	
 			if ((*it)->getType() == type->getType())
 				return ;
+		}
 		_types.push_back(type->clone());
 	}
 }
